7114ac15a787df8e237b4cbc15950d515d0c1ea3
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-04-22 18:35:23 +0100

    version secure

--- src/main/java/com/sip/ams/GestionDeStockRestApplication.java
+++ src/main/java/com/sip/ams/GestionDeStockRestApplication.java
@@ -2,9 +2,10 @@ package com.sip.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestionDeStockRestApplication {
+public class GestionDeStockRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionDeStockRestApplication.class, args);
